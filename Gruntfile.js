f66855a7dd32944a4f8060ae6d42410649d505b2
Mickaël Borne <mickael.borne@ign.fr>
2016-02-09 18:03:53 +0100

    ajout de la notion de bundle

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,19 @@ module.exports = function(grunt) {
             }
         },
 
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['bower_components/leaflet-draw/dist/images/*.png'],
+                        dest: 'dist/images'
+                    }
+                ]
+            }
+        },
+
         concat: {
             options: {
                 separator: ';',
@@ -18,12 +31,29 @@ module.exports = function(grunt) {
             dist: {
                 src: ['src/**/*.js'],
                 dest: 'dist/jquery-geometry-editor.js',
+            },
+            "bundle-js": {
+                src: [
+                    'bower_components/leaflet/dist/leaflet.js',
+                    'bower_components/leaflet-draw/dist/leaflet.draw.js',
+                    'bower_components/leaflet-omnivore/leaflet-omnivore.min.js',
+                    'src/**/*.js'
+                ],
+                dest: 'dist/bundle.js',
+            },
+            "bundle-css": {
+                src: [
+                    'bower_components/leaflet/dist/leaflet.css',
+                    'bower_components/leaflet-draw/dist/leaflet.draw.css'
+                ],
+                dest: 'dist/bundle.css',
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['jshint', 'concat']);
+    grunt.registerTask('default', ['jshint', 'copy', 'concat']);
 };
